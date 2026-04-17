875d1044acc7b7891181e45c99a6c9ae17647d1b
Kik02 <Golovin.Savelii@yandex.ru>
2026-04-17 20:18:45 +0500

    разработанный модель данных шапавалав

---
+++ network/src/main/java/com/example/network/domains/common/Settings.java
@@ -0,0 +1,7 @@
+package com.example.network.domains.common;
+
+public class Settings {
+    public static String URL = "http://10.0.2.2:5000";
+
+    public static String DEMO_TOKEN = "22e2c951-414f-4e70-a09b-305e4a637ad2";
+}
