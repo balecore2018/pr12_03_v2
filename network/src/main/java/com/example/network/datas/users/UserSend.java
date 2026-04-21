8aad2336879dc3fb85636cfd66697c2260187af3
Kik02 <Golovin.Savelii@yandex.ru>
2026-04-21 14:19:08 +0500

    уведомления на почту Шаповалов

---
+++ network/src/main/java/com/example/network/datas/users/UserSend.java
@@ -0,0 +1,34 @@
+package com.example.network.datas.users;
+
+import com.example.network.domains.apis.MyAsyncTask;
+import com.example.network.domains.callbacks.MyResponseCallback;
+import com.example.network.domains.common.Settings;
+import com.example.network.domains.models.User;
+
+import java.io.IOException;
+import java.sql.Connection;
+
+public class UserSend extends MyAsyncTask {
+    String mail;
+
+    public UserSend(String mail, MyResponseCallback callback) {
+        super(callback);
+        this.mail = mail;
+    }
+
+    @Override
+    protected String doInBackground(Void... voids) {
+        try {
+            Connection.Response response = Jsoup.connect(Settings.URL + "/api/user/send?email=" + mail)
+                    .ignoreContentType(true)
+                    .ignoreHttpErrors(true)
+                    .method(Connection.Method.GET)
+                    .execute();
+            return response.statusCode() == 200 ?
+                    response.body() :
+                    "Error: " + response.body();
+        } catch (IOException e) {
+            return "Error: " + e.getMessage();
+        }
+    }
+}
