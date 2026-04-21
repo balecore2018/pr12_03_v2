2cc933e59e4f00e1d273bf404a1e60e9d9f5e6d7
Kik02 <Golovin.Savelii@yandex.ru>
2026-04-21 14:16:05 +0500

    юзергет сделал Шаповалов

---
+++ network/src/main/java/com/example/network/datas/users/UserGet.java
@@ -0,0 +1,36 @@
+package com.example.network.datas.users;
+
+import com.example.network.domains.apis.MyAsyncTask;
+import com.example.network.domains.callbacks.MyResponseCallback;
+import com.example.network.domains.common.Settings;
+import com.example.network.domains.models.User;
+
+import java.io.IOException;
+import java.sql.Connection;
+
+public class UserGet extends MyAsyncTask {
+    String token;
+
+    public UserGet(String token, MyResponseCallback callback) {
+        super(callback);
+        this.token = token;
+    }
+
+    @Override
+    protected String doInBackground(Void... voids) {
+        try {
+            Connection.Response response = Jsoup.connect(Settings.URL + "/api/user/get")
+                    .ignoreContentType(true)
+                    .ignoreHttpErrors(true)
+                    .method(Connection.Method.GET)
+                    .header("Content-type", "application/json")
+                    .header("token", token)
+                    .execute();
+            return response.statusCode() == 200 ?
+                    response.body() :
+                    "Error: " + response.body();
+        } catch (IOException e) {
+            return "Error: " + e.getMessage();
+        }
+    }
+}
